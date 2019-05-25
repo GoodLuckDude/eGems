0df9c70f059281da8cc2d756a4449fa8b869d2e5
goodLuckDude <tsirkmaks@mail.ru>
2019-05-25 10:25:30 +0500

    profile's start

---
+++ _parts/navbar.php
@@ -0,0 +1,8 @@
+<nav class="site-header">
+  <div class="container-fluid d-flex justify-content-between">
+    <a class="" href="#">Ваш профиль</a>
+    <a class="" href="#">Драгоценности</a>
+    <a class="" href="#">Пользователи</a>
+    <a id="exit" class="" href="#">Выход</a>
+  </div>
+</nav>
--- dwarf/profile.php
+++ dwarf/profile.php
@@ -5,4 +5,50 @@ require_once '../func/common.php';
 session_start();
 redirIfGuest();
 
+include_once "../_parts/header.php"
 ?>
+
+<body>
+<?php include_once "../_parts/navbar.php" ?>
+
+<div id="preview" class="container">
+  <div id="" class="row justify-content-center">
+    <div class="col-4 img">
+      <img src="../img/dwarf.png" width="300px" height="300px" alt="Картинка гнома">
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-4 name">
+      <span>Имя пользователя</span>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-2 race">
+      <span>Раса</span>
+    </div>
+  </div>
+</div>
+
+<div id="mined-gems" class="container">
+  <div class="row justify-content-center">
+    <span>Добыл всего</span>
+  </div>
+</div>
+
+<div id="other-data" class="container">
+  <div class="row justify-content-around">
+    <div class="col col-3">Email: </div>
+    <div class="col col-5"><label for="change-email" class="lable">em***@email.ru</label></div>
+    <div class="col col-2"><button id="change-email" class="button" type="button">Изменить</button></div>
+  </div>
+  
+  <div class="row justify-content-around">
+    <div class="col col-3">Пароль:</div>
+    <div class="col col-5"><label for="change-password" class="lable">********</label></div>
+    <div class="col col-2"><button id="change-password" class="button" type="button">Изменить</button></div>
+  </div>
+</div>
+
+</body>
